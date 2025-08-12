6887a9dc9fe60a44ff9c9e4cf5866e0fa56de8a0
Iago Developer <iago.developer01@gmail.com>
2025-08-12 20:07:20 -0300

    first commit

---
+++ src/main/java/umc/pfc/orientamais/OrientamaisApplication.java
@@ -0,0 +1,13 @@
+package umc.pfc.orientamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/pfc/orientamais/OrientamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.pfc.orientamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
