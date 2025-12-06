b9fb1d8e8b6bd20a1a04f13bf836e75f14044b4f
Mateus Teixeira <mateus.programacao29@gmail.com>
2025-12-06 17:09:51 -0300

    fix: validação de start time no list all

--- src/main/java/umc/pfc/orientamais/application/service/LessonService.java
+++ src/main/java/umc/pfc/orientamais/application/service/LessonService.java
@@ -31,6 +31,7 @@ import umc.pfc.orientamais.domain.model.mentor.Mentor;
 import umc.pfc.orientamais.domain.model.mentored.Mentored;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -176,6 +177,9 @@ public class LessonService implements LessonUseCase {
 
         Pageable pageable = PageRequest.of(page, size, getSort(order));
         Specification<Lesson> spec = (root, query, cb) -> cb.conjunction();
+
+        spec = spec.and((root, query, cb) ->
+                cb.greaterThan(root.get("startTime"), LocalDateTime.now()));
 
         if (title != null && !title.isBlank()) {
             spec = spec.and((root, query, cb) ->
