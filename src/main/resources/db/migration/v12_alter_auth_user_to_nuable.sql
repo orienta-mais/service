c1058ea2ae1c04a8aece543385bf55e540534e69
João Godoi <joaogodoi.dev@gmail.com>
2025-12-07 21:35:05 -0300

    hotfix: fix conflicts

--- src/main/resources/db/migration/v12_alter_auth_user_to_nuable.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE auth_user
-ALTER COLUMN email DROP NOT NULL,
-ALTER COLUMN password DROP NOT NULL;
-
-ALTER TABLE mentor
-ADD COLUMN active boolean NOT NULL DEFAULT true;
-
-alter table mentor
-alter column user_uuid DROP NOT null;
